28d66dfc74d74245ff640731440d289cbcfe6e5a
qq378881955 <378881955@qq.com>
2020-04-30 19:59:17 +0800

    111

---
+++ qq/src/main/java/com/qq/qq/QqApplication.java
@@ -0,0 +1,14 @@
+package com.qq.qq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class QqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QqApplication.class, args);
+	}
+
+}
